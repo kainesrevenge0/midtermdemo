ab825c9c566fdde76c5290e2dd12bae4b9d15339
kainesrevenge0 <brother26111@gmail.com>
2026-03-14 21:43:20 -0400

    Spring Boot midterm project

---
+++ src/main/java/com/example/midtermdemo/MidtermdemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.midtermdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MidtermdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MidtermdemoApplication.class, args);
+	}
+
+}
